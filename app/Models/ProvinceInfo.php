3e9ba4782087c872d8060f453cefb5a9a74418ed
Natsu1st <98305416+Natsu1st@users.noreply.github.com>
2023-05-02 01:20:52 +0545

    Add files via upload

---
+++ app/Models/ProvinceInfo.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProvinceInfo extends Model
+{
+    use HasFactory;
+    protected $fillable = ['province_id','province_name','phone'];
+    
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function users(){
+        return $this->hasManyThrough(UserProfile::class,User::class);
+    }
+    public function profile(){
+        return $this->belongsTo(UserProfile::class,'province_id','province_id');
+    }
+}
---
+++ app/Models/UserProfile.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserProfile extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['province_id','user_id','phone','first_name','last_name','dob','gender','nationality','status','grandfather_name','father_name','mother_name','spouse_name','citizenship_numer','citizenship_issue_date','citizenship_documents','country','province','district','municipality','ward','tole'];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+    public function province(){
+        return $this->hasOne(ProvinceInfo::class,'province_id','province_id');
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
